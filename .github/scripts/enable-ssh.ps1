50ad15928b31cc934ad21412dd8d317aab5eca30
GregYS <74958778+GregYS@users.noreply.github.com>
2025-07-31 12:45:28 +0300

    Update enable-ssh.ps1

--- .github/scripts/enable-ssh.ps1
+++ .github/scripts/enable-ssh.ps1
@@ -1,1 +1,5 @@
-
+<powershell>
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+</powershell>
